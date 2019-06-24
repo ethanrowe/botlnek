cbcc8d873f0526fc001c1695870a246f4f6e82d3
Ethan Rowe <ethanrowe000@gmail.com>
2019-06-24 16:26:47 -0400

    Some stubbings out of REST and models

--- pkg/model/objects.go
+++ pkg/model/objects.go
@@ -2,9 +2,28 @@ package model
 
 import (
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"github.com/ethanrowe/botlnek/pkg/util"
+	"time"
 )
+
+// The specific counter implementation details come
+// from the persistence layer, so we simply want an
+// interface that makes sense.
+type Counter interface {
+	// -1 if a < b, 0 of a == b, 1 if a > b
+	Cmp(a, b Counter) int
+	// a < b; for the sort package
+	Less(a, b Counter) bool
+	json.Marshaler
+	json.Unmarshaler
+}
+
+type ClockEntry struct {
+	Count       Counter
+	Approximate time.Time
+}
 
 type Source struct {
 	Keys     map[string]string
@@ -16,4 +35,24 @@ func (s Source) KeyHash() string {
 	hash := sha256.New()
 	pairs.WriteTo(hash)
 	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+type SourceRegistration struct {
+	ClockEntry
+	Source
+}
+
+type SourceRegistrations map[string]SourceRegistration
+
+type SourceMap map[string]SourceRegistrations
+
+type Partition struct {
+	Key     string
+	Attrs   util.StringKVPairs
+	Sources SourceMap
+}
+
+type Domain struct {
+	Attrs      util.StringKVPairs
+	Partitions map[string]Partition
 }
--- pkg/rest/routes.go
+++ pkg/rest/routes.go
@@ -1,7 +1,10 @@
 package rest
 
 import (
+	"errors"
+	"github.com/ethanrowe/botlnek/pkg/util"
 	"net/http"
+	"strings"
 )
 
 func HelloWorldRoute(r *http.Request) (JsonResponder, error) {
@@ -17,10 +20,60 @@ func HelloWorldRoute(r *http.Request) (JsonResponder, error) {
 	return NewJsonResponse(200, result), nil
 }
 
+func DomainsCollectionRoute(r *http.Request) (JsonResponder, error) {
+	if r.Method == http.MethodGet {
+		// For now we'll always respond with an empty list.
+		return NewJsonResponse(200, struct{ Domains []string }{make([]string, 0)}), nil
+	}
+	if r.Method == http.MethodPost {
+		// For now stub with a bogus created.
+		return NewJsonResponse(202, "ok"), nil
+	}
+	return NewJsonErrorResponse(http.StatusMethodNotAllowed, errors.New("Only GET/POST are supported")), nil
+}
+
+func DomainRoute(r *http.Request) (JsonResponder, error) {
+	if r.Method == http.MethodGet {
+		// For now respond with a stub domain
+		domain := struct {
+			DomainKey string
+			Attrs     util.StringKVPairs
+		}{
+			strings.TrimPrefix(r.URL.Path, "/domains/"),
+			util.NewStringKVPairs(
+				map[string]string{
+					"foo": "Foo?",
+					"bar": "Bar!",
+					"baz": "baz...",
+				},
+			),
+		}
+
+		return NewJsonResponse(200, domain), nil
+	}
+	return NewJsonErrorResponse(http.StatusMethodNotAllowed, errors.New("Only GET is supported")), nil
+}
+
+func PartitionsRoute(r *http.Request) (JsonResponder, error) {
+	if r.Method == http.MethodPost {
+		// For now just accept.
+		return NewJsonResponse(202, "partition accepted"), nil
+	}
+	return NewJsonErrorResponse(http.StatusMethodNotAllowed, errors.New("Only POST supported")), nil
+}
+
 func HandleJsonRoute(mux *http.ServeMux, pattern string, h func(*http.Request) (JsonResponder, error)) {
 	mux.Handle(pattern, NewJsonHandler(h))
 }
 
 func ApplyRoutes(mux *http.ServeMux) {
 	HandleJsonRoute(mux, "/", HelloWorldRoute)
+	// Get a list of domains;
+	// Post a new domain
+	HandleJsonRoute(mux, "/domains", DomainsCollectionRoute)
+	// Get a specific domain
+	HandleJsonRoute(mux, "/domains/", DomainRoute)
+	// Post a new partition
+	// Get an existing partition
+	HandleJsonRoute(mux, "/partitions/", PartitionsRoute)
 }
