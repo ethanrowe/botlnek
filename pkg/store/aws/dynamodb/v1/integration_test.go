7988823ca4b1e944762be9f855e93a8a9e92576e
Ethan Rowe <ethanrowe000@gmail.com>
2019-09-05 16:49:39 -0400

    DynamoDB store AppendNewSource impl for realz
    Expands the integration test to cover a few source append scenarios:
    * Sources to a new aggregate
    * Sources to an existing aggregate
    * Sources that conflict with existing sources in an aggregate (the
      successful no-op case)
    
    And gets the actual AppendNewSource method working (without retries
    or the like) so all integration tests pass.

--- pkg/store/aws/dynamodb/v1/integration_test.go
+++ pkg/store/aws/dynamodb/v1/integration_test.go
@@ -5,12 +5,14 @@ package v1
 import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 	"github.com/ethanrowe/botlnek/pkg/model"
 	"github.com/ethanrowe/botlnek/pkg/util"
 	"reflect"
+	"sort"
 	"testing"
 	"time"
 )
@@ -57,7 +59,7 @@ func (s *dynamoDBSession) Next() string {
 }
 
 func (s *dynamoDBSession) BuildTable(table string, pk string, sk string) {
-	result, err := s.service.CreateTable(
+	_, err := s.service.CreateTable(
 		&dynamodb.CreateTableInput{
 			KeySchema: []*dynamodb.KeySchemaElement{
 				{
@@ -89,7 +91,6 @@ func (s *dynamoDBSession) BuildTable(table string, pk string, sk string) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Table build result for %s: %v\n", table, result)
 }
 
 type testScenario struct {
@@ -126,7 +127,19 @@ func (ts *testScenario) finisher() {
 
 // no op for now
 func (ts *testScenario) teardown() {
-	return
+	_, err := ts.Session.service.DeleteTable(&dynamodb.DeleteTableInput{
+		TableName: aws.String(ts.Name),
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			if aerr.Code() == dynamodb.ErrCodeResourceNotFoundException {
+				err = nil
+			}
+		}
+	}
+	if err != nil {
+		fmt.Println("Teardown of", ts.Name, "resulted in error:", err)
+	}
 }
 
 func (ts *testScenario) BatchWriteInput(items ...*dynamodb.WriteRequest) *dynamodb.BatchWriteItemInput {
@@ -135,6 +148,11 @@ func (ts *testScenario) BatchWriteInput(items ...*dynamodb.WriteRequest) *dynamo
 			ts.Name: items,
 		},
 	}
+}
+
+func (ts *testScenario) Query(query dynamodb.QueryInput) (*dynamodb.QueryOutput, error) {
+	query.TableName = aws.String(ts.Name)
+	return ts.Session.service.Query(&query)
 }
 
 func (ts *testScenario) PrepareSourceRevision(pk, revk, srck, rev string, tstamp time.Time, keys, attrs *dynamodb.AttributeValue) (*dynamodb.BatchWriteItemOutput, error) {
@@ -180,6 +198,126 @@ func (ts *testScenario) PrepareSourceRevision(pk, revk, srck, rev string, tstamp
 		},
 	)
 	return ts.Session.service.BatchWriteItem(input)
+}
+
+func (ts *testScenario) PutItems(items []map[string]*dynamodb.AttributeValue) error {
+	inputs := make([]*dynamodb.WriteRequest, len(items))
+	for i, item := range items {
+		inputs[i] = &dynamodb.WriteRequest{
+			PutRequest: &dynamodb.PutRequest{
+				Item: item,
+			},
+		}
+	}
+	_, err := ts.Session.service.BatchWriteItem(ts.BatchWriteInput(inputs...))
+	return err
+}
+
+func (ts *testScenario) PrepareLogExpectation(pk, revk, rev string, tstamp ...time.Time) map[string]*dynamodb.AttributeValue {
+	out := map[string]*dynamodb.AttributeValue{
+		ts.Store.AggregateKeyColumn: {
+			S: aws.String(pk),
+		},
+		ts.Store.AggregateMemberKeyColumn: {
+			S: aws.String(revk),
+		},
+		"rev": {
+			N: aws.String(rev),
+		},
+	}
+	if len(tstamp) > 1 {
+		panic("Only one timestamp may be provided")
+	}
+	if len(tstamp) == 1 {
+		out["ts"] = &dynamodb.AttributeValue{
+			S: aws.String(tstamp[0].Format("20060102T150405.999")),
+		}
+	}
+	return out
+}
+
+func (ts *testScenario) PrepareSourceExpectation(pk, mk, rev string, keys, attrs map[string]string) map[string]*dynamodb.AttributeValue {
+	k := make(map[string]*dynamodb.AttributeValue)
+	a := make(map[string]*dynamodb.AttributeValue)
+	for key, val := range keys {
+		k[key] = &dynamodb.AttributeValue{S: aws.String(val)}
+	}
+	for key, val := range attrs {
+		a[key] = &dynamodb.AttributeValue{S: aws.String(val)}
+	}
+	return map[string]*dynamodb.AttributeValue{
+		ts.Store.AggregateKeyColumn: {
+			S: aws.String(pk),
+		},
+		ts.Store.AggregateMemberKeyColumn: {
+			S: aws.String(mk),
+		},
+		"rev": {
+			N: aws.String(rev),
+		},
+		"km": {
+			M: k,
+		},
+		"am": {
+			M: a,
+		},
+	}
+}
+
+// The aggregate rows in item group `pk` match up (not just in provided properties,
+// but in row count and order).
+func (ts *testScenario) VerifyAllAggregateItems(pk string, items []map[string]*dynamodb.AttributeValue) error {
+	qry, err := ts.Query(dynamodb.QueryInput{
+		KeyConditionExpression: aws.String(fmt.Sprintf("%s = :v1", ts.Store.AggregateKeyColumn)),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":v1": {
+				S: aws.String(pk),
+			},
+		},
+		ConsistentRead: aws.Bool(true),
+	})
+	if err != nil {
+		return err
+	}
+	if aws.Int64Value(qry.Count) != int64(len(items)) {
+		return fmt.Errorf("expected %d items; found %d", len(items), aws.Int64Value(qry.Count))
+	}
+
+	for i, item := range items {
+		got := qry.Items[i]
+		for key, val := range item {
+			rec, ok := got[key]
+			if !ok {
+				return fmt.Errorf("item %d expected attr %s not found", i, key)
+			}
+			if !reflect.DeepEqual(val, rec) {
+				return fmt.Errorf("item %d attr %s expected %v but received %v", i, key, val, rec)
+			}
+		}
+	}
+	return nil
+}
+
+func (ts *testScenario) SortedSourceItems(items []map[string]*dynamodb.AttributeValue) []map[string]*dynamodb.AttributeValue {
+	out := make([]map[string]*dynamodb.AttributeValue, len(items))
+	copy(out, items)
+	sorter := func(i, j int) bool {
+		return aws.StringValue(out[i][ts.Store.AggregateMemberKeyColumn].S) < aws.StringValue(out[j][ts.Store.AggregateMemberKeyColumn].S)
+	}
+	sort.Slice(out, sorter)
+	return out
+}
+
+func itemConcat(items ...[]map[string]*dynamodb.AttributeValue) []map[string]*dynamodb.AttributeValue {
+	length := 0
+	for _, item := range items {
+		length = length + len(item)
+	}
+	result := make([]map[string]*dynamodb.AttributeValue, 0, length)
+	for _, item := range items {
+		result = append(result, item...)
+	}
+	return result
 }
 
 func TestRead(t *testing.T) {
@@ -254,7 +392,6 @@ func TestRead(t *testing.T) {
 		t.Fatalf("Expected aggregate with 1 collection key; got %d keys", len(received.Sources))
 	}
 
-	fmt.Printf("Aggregate: %v\n", received)
 	colSrcs, ok := received.Sources["groupA"]
 	if !ok {
 		t.Fatal("Expected to find collection 'groupA'; not found.")
@@ -380,5 +517,203 @@ func TestRead(t *testing.T) {
 		if !reflect.DeepEqual(testpair.Expect, testpair.Received) {
 			t.Errorf("%s source mismatch; expected vs got:\n\t%v\n\t%v", testpair.Label, testpair.Expect, testpair.Received)
 		}
+	}
+}
+
+// Writing source for a new aggregate
+func TestNewAggregateWrite(t *testing.T) {
+	sess := newDynamoDBSession("basic-write")
+	scenario := sess.NewScenario()
+	defer scenario.Finish()
+	dk := model.DomainKey("empty")
+	ak := model.AggregateKey("new")
+	colk := "some-collection"
+
+	var store model.AggregateWriter
+
+	// Cast as an aggregate writer since that's how we're using it.
+	store = scenario.Store
+
+	source := model.Source{
+		Keys: map[string]string{
+			"foo-key": "foo value",
+			"bar-key": "bar value",
+		},
+		Attrs: map[string]string{
+			"baz-attr":  "baz value",
+			"bink-attr": "bink value",
+		},
+	}
+
+	// We should get no error, and respSource should match source, because
+	// it's new.
+	respSource, respErr := store.AppendNewSource(dk, ak, colk, source)
+
+	if respErr != nil {
+		t.Fatalf("Failed inserting source: %s", respErr)
+	}
+
+	if respSource == nil {
+		t.Fatal("Expected non-nil source response")
+	}
+
+	if !reflect.DeepEqual(source, *respSource) {
+		t.Errorf("Source response mismatch, expected vs received:\n\t%v\n\t%v", source, *respSource)
+	}
+
+	// Now let's consider the state in the table.
+	// partition key is domain key and agg key in json list
+	// source sort key is collection key and source keyhash in json list
+	pk := fmt.Sprintf("[%q,%q]", string(dk), string(ak))
+	sk := fmt.Sprintf("[%q,%q]", colk, source.KeyHash())
+	expectedItems := []map[string]*dynamodb.AttributeValue{
+		scenario.PrepareLogExpectation(pk, "R0000000000", "0"),
+		scenario.PrepareSourceExpectation(pk, sk, "0", source.Keys, source.Attrs),
+	}
+
+	err := scenario.VerifyAllAggregateItems(pk, expectedItems)
+	if err != nil {
+		t.Errorf("Expected dynamodb item failure: %s", err)
+	}
+}
+
+// Writing source for an existing aggregate
+func TestExistingAggregateWrite(t *testing.T) {
+	sess := newDynamoDBSession("existing-write")
+	scenario := sess.NewScenario()
+	defer scenario.Finish()
+	dk := model.DomainKey("extant")
+	ak := model.AggregateKey("existing")
+
+	sourceKey := func(groupkey, sourcekey string) string {
+		return fmt.Sprintf("[%q,%q]", groupkey, sourcekey)
+	}
+
+	// Cast as an aggregate writer since that's how we're using it.
+	var store model.AggregateWriter
+	store = scenario.Store
+
+	source1 := model.Source{
+		Keys: map[string]string{
+			"I":       "like",
+			"traffic": "lights",
+		},
+		Attrs: map[string]string{
+			"That": "is",
+			"what": "I said",
+		},
+	}
+
+	source2 := model.Source{
+		Keys: map[string]string{
+			"but":  "not",
+			"when": "they",
+		},
+		Attrs: map[string]string{
+			"are": "red",
+		},
+	}
+
+	// The first two logs will have explicit timestamps because we're prepping
+	// them in dynamodb directly; the others get indeterminate timestamps we'll
+	// check later.
+	pk := fmt.Sprintf("[%q,%q]", string(dk), string(ak))
+	expectedLogItems := []map[string]*dynamodb.AttributeValue{
+		scenario.PrepareLogExpectation(pk, "R0000000000", "0", time.Now().UTC().Add(-time.Minute).Round(time.Millisecond).Add(5*time.Millisecond)),
+		scenario.PrepareLogExpectation(pk, "R0000000001", "1", time.Now().UTC().Add(-30*time.Second).Round(time.Millisecond).Add(-5*time.Millisecond)),
+		scenario.PrepareLogExpectation(pk, "R0000000002", "2"),
+		scenario.PrepareLogExpectation(pk, "R0000000003", "3"),
+	}
+
+	// The first two guys are the same source in different collections,
+	// prepped in dynamodb.  The second two guys are what we expect after
+	// appending the second source to those collections.
+	expectedSourceItems := []map[string]*dynamodb.AttributeValue{
+		scenario.PrepareSourceExpectation(pk, sourceKey("group-a", source1.KeyHash()), "0", source1.Keys, source1.Attrs),
+		scenario.PrepareSourceExpectation(pk, sourceKey("group-b", source1.KeyHash()), "1", source1.Keys, source1.Attrs),
+		scenario.PrepareSourceExpectation(pk, sourceKey("group-a", source2.KeyHash()), "2", source2.Keys, source2.Attrs),
+		scenario.PrepareSourceExpectation(pk, sourceKey("group-b", source2.KeyHash()), "3", source2.Keys, source2.Attrs),
+	}
+
+	err := scenario.PutItems(itemConcat(expectedLogItems[:2], expectedSourceItems[:2]))
+	if err != nil {
+		t.Fatalf("Failed to prepare items in dynamo: %s", err)
+	}
+
+	// We'll separate our append attempts by some time so we can verify
+	// that the timestamps have a reasonable relationship after the fact.
+	for _, grp := range []string{"group-a", "group-b"} {
+		time.Sleep(100 * time.Millisecond)
+		res, err := store.AppendNewSource(dk, ak, grp, source2)
+
+		if err != nil {
+			t.Errorf("Failed to append second source to %s: %s", grp, err)
+		}
+
+		if res == nil {
+			t.Errorf("expected non-nil source for append to %s", grp)
+		} else if !reflect.DeepEqual(source2, *res) {
+			t.Errorf("%s append of second source mismatch (expect vs receive):\n\t%v\n\t%v", grp, source2, res)
+		}
+	}
+
+	// Now check the dynamodb state against the expectations.
+	err = scenario.VerifyAllAggregateItems(pk, itemConcat(expectedLogItems, scenario.SortedSourceItems(expectedSourceItems)))
+	if err != nil {
+		t.Fatalf("Expected dynamodb item failure: %s", err)
+	}
+
+	// If we're here, state lines up with expectations, so we just need to
+	// check the timestampiness of the last two logs (that were created by
+	// our append operations).
+}
+
+// Writing source that already exists in target aggregate
+func TestConflictingWrite(t *testing.T) {
+	sess := newDynamoDBSession("conflicting-write")
+	scenario := sess.NewScenario()
+	defer scenario.Finish()
+	dk := model.DomainKey("existing")
+	ak := model.AggregateKey("conflicting")
+	colk := "mygroup"
+
+	// Cast as an aggregate writer since that's how we're using it.
+	var store model.AggregateWriter
+	store = scenario.Store
+
+	source := model.Source{
+		Keys: map[string]string{
+			"Eennie":  "Meennie",
+			"Meinnie": "Moe",
+		},
+		Attrs: map[string]string{
+			"Not": "Relevant",
+		},
+	}
+
+	pk := fmt.Sprintf("[%q,%q]", string(dk), string(ak))
+	sk := fmt.Sprintf("[%q,%q]", colk, source.KeyHash())
+	expectedItems := []map[string]*dynamodb.AttributeValue{
+		scenario.PrepareLogExpectation(pk, "R0000000000", "0", time.Now().UTC().Round(time.Millisecond)),
+		scenario.PrepareSourceExpectation(pk, sk, "0", source.Keys, map[string]string{"Don't": "matter"}),
+	}
+
+	err := scenario.PutItems(expectedItems)
+	if err != nil {
+		t.Fatalf("Failed to prepare items in dynamo: %s", err)
+	}
+
+	srcOut, err := store.AppendNewSource(dk, ak, colk, source)
+	if err != nil {
+		t.Errorf("Error reported appending source: %s", err)
+	}
+
+	if srcOut != nil {
+		t.Error("Expected nil source, but received non-nil result")
+	}
+
+	err = scenario.VerifyAllAggregateItems(pk, expectedItems)
+	if err != nil {
+		t.Errorf("Dynamodb state mismatch: %s", err)
 	}
 }
--- pkg/store/aws/dynamodb/v1/store.go
+++ pkg/store/aws/dynamodb/v1/store.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ethanrowe/botlnek/pkg/model"
 	"github.com/ethanrowe/botlnek/pkg/store/inmemory"
 	"strconv"
+	"time"
 )
 
 const (
@@ -16,6 +17,8 @@ const (
 	ColNameKeys      = "km"
 	ColNameAttrs     = "am"
 	ColNameTimestamp = "ts"
+	RevSortKeyMin    = "R0000000000"
+	RevSortKeyMax    = "R9999999999"
 )
 
 type DynamoDbStoreV1 struct {
@@ -36,6 +39,11 @@ func NewStore(service dynamodbiface.DynamoDBAPI, tablename string) *DynamoDbStor
 
 func (store *DynamoDbStoreV1) HashKey(dk model.DomainKey, ak model.AggregateKey) string {
 	data, _ := json.Marshal([]string{string(dk), string(ak)})
+	return string(data)
+}
+
+func (store *DynamoDbStoreV1) SourceSortKey(colKey string, source model.Source) string {
+	data, _ := json.Marshal([]string{colKey, source.KeyHash()})
 	return string(data)
 }
 
@@ -69,8 +77,109 @@ func (store *DynamoDbStoreV1) GetAggregate(dk model.DomainKey, ak model.Aggregat
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("Result from query: %v", result)
 
 	_ = reader.IngestItems(result)
 	return reader.Aggregate, reader.Error
+}
+
+func (store *DynamoDbStoreV1) GetNextRev(hashKey *string) (next int, err error) {
+	resp, err := store.Service.Query(&dynamodb.QueryInput{
+		TableName:              aws.String(store.TableName),
+		KeyConditionExpression: aws.String("#H = :hk AND #S BETWEEN :smin AND :smax"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":hk":   {S: hashKey},
+			":smin": {S: aws.String(RevSortKeyMin)},
+			":smax": {S: aws.String(RevSortKeyMax)},
+		},
+		ExpressionAttributeNames: map[string]*string{
+			"#H": aws.String(store.AggregateKeyColumn),
+			"#S": aws.String(store.AggregateMemberKeyColumn),
+		},
+		Limit:                aws.Int64(1),
+		ScanIndexForward:     aws.Bool(false),
+		ProjectionExpression: aws.String("rev"),
+	})
+	if err == nil {
+		if aws.Int64Value(resp.Count) > 0 {
+			next, err = strconv.Atoi(aws.StringValue(resp.Items[0]["rev"].N))
+			if err == nil {
+				next++
+			}
+		}
+	}
+	return
+}
+
+func (store *DynamoDbStoreV1) AppendNewSource(dk model.DomainKey, ak model.AggregateKey, colkey string, source model.Source) (*model.Source, error) {
+	hashKey := aws.String(store.HashKey(dk, ak))
+	nextRev, err := store.GetNextRev(hashKey)
+	if err != nil {
+		return nil, err
+	}
+	revSort := aws.String(fmt.Sprintf("R%010d", nextRev))
+	revVal := &dynamodb.AttributeValue{N: aws.String(fmt.Sprintf("%d", nextRev))}
+	resp, err := store.Service.TransactWriteItems(&dynamodb.TransactWriteItemsInput{
+		TransactItems: []*dynamodb.TransactWriteItem{
+			{
+				Put: &dynamodb.Put{
+					TableName:           aws.String(store.TableName),
+					ConditionExpression: aws.String("attribute_not_exists(#S)"),
+					ExpressionAttributeNames: map[string]*string{
+						"#S": aws.String(store.AggregateMemberKeyColumn),
+					},
+					Item: map[string]*dynamodb.AttributeValue{
+						store.AggregateKeyColumn: {
+							S: hashKey,
+						},
+						store.AggregateMemberKeyColumn: {
+							S: revSort,
+						},
+						"rev": revVal,
+						"ts":  ToDynamoMillisTimestamp(time.Now()),
+					},
+				},
+			},
+			{
+				Put: &dynamodb.Put{
+					TableName:           aws.String(store.TableName),
+					ConditionExpression: aws.String("attribute_not_exists(#S)"),
+					ExpressionAttributeNames: map[string]*string{
+						"#S": aws.String(store.AggregateMemberKeyColumn),
+					},
+					Item: map[string]*dynamodb.AttributeValue{
+						store.AggregateKeyColumn: {
+							S: hashKey,
+						},
+						store.AggregateMemberKeyColumn: {
+							S: aws.String(store.SourceSortKey(colkey, source)),
+						},
+						"rev": revVal,
+						"km":  ToDynamoStringMap(source.Keys),
+						"am":  ToDynamoStringMap(source.Attrs),
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		mpe, perr := NewAwsMultipartException(err.Error())
+		if perr == nil {
+			// If the second item's condition failed, that means
+			// the item already exists, which is a successful result.
+			// For this situation, the first item's success/failure
+			// is irrelevant.
+			if mpe.ExceptionParts[1] == AwsExcTypeConditionalCheckFailed {
+				// The source should be nil in this case,
+				// because we didn't mutate anything.
+				return nil, nil
+			}
+
+		} else {
+			fmt.Println("Problem parsing MPE:", perr)
+			fmt.Println("ERROR", err)
+		}
+		return nil, err
+	}
+	fmt.Println("Got dynamo insert responses:", resp)
+	return &source, nil
 }
